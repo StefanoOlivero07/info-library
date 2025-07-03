6df9014ea0ee2ab88f3f6bd54bb94dbf4c8a0566
Stefano Olivero <s.olivero.3215@vallauri.edu>
2025-07-03 17:53:02 +0200

    Created test project and developed CreateMenu method

---
+++ Library/Library.Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Library.Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+
+            do
+            {
+                CreateMenu();
+                choice = Console.ReadKey().KeyChar;
+
+                switch (choice)
+                {
+                    case 'A':
+                        Console.WriteLine("Hi!");
+                        break;
+                    default:
+                        Console.WriteLine("This choice does not exist");
+                        break;
+                }
+            } while (choice.ToString().ToUpper() != "Q");
+
+            Console.ReadKey();
+        }
+
+        public static void CreateMenu()
+        {
+            Console.WriteLine("------------------- Info Library - Test -------------------");
+            Console.WriteLine("\n");
+            Console.WriteLine("A - Get all users");
+            Console.WriteLine("B - Get user by id");
+            Console.WriteLine("C - Create user");
+            Console.WriteLine("D - Delete user");
+            Console.WriteLine("------------------------------------------------------------");
+            Console.WriteLine("Q - Exit");
+            Console.WriteLine("\nEnter a choice: ");
+        }
+    }
+}
