f7b1551ba2753dab21aaf467629b07e826b4ba75
Stefano Olivero <s.olivero.3215@vallauri.edu>
2025-07-03 17:35:01 +0200

    Add Database class and UserRepository

---
+++ Library/Library.Data/Database.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Library.Data
+{
+    public class Database
+    {
+        private readonly string _connectionString;
+
+        public Database(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public SqlDataReader ExecuteReader(string query, SqlParameter[]? parameters = null)
+        {
+            var connection = GetConnection();
+            using var command = new SqlCommand(query, connection);
+
+            if (parameters != null)
+                command.Parameters.AddRange(parameters);
+            connection.Open();
+
+            return command.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+
+        public int ExecuteNonQuery(string query, SqlParameter[]? parameters = null)
+        {
+            using var connection = GetConnection();
+            using var command = new SqlCommand(query, connection);
+
+            if (parameters != null)
+                command.Parameters.AddRange(parameters);
+            connection.Open();
+
+            return command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Library/Library.Data/Repos/UserRepository.cs
@@ -0,0 +1,104 @@
+﻿using Library.Core.Models;
+using Microsoft.Data.SqlClient;
+
+namespace Library.Data.Repos
+{
+    public class UserRepository
+    {
+        private readonly Database _db;
+
+        public UserRepository(string connStr)
+        {
+            _db = new Database(connStr);
+        }
+
+        public List<User> GetAll()
+        {
+            var users = new List<User>();
+            string query = "SELECT * Users";
+            using var reader = _db.ExecuteReader(query);
+
+            while (reader.Read())
+            {
+                users.Add(new User
+                {
+                    Id = reader.GetInt32(0),
+                    DateOfBirth = reader.GetDateTime(1),
+                    Name = reader.GetString(2),
+                    Surname = reader.GetString(3),
+                    Email = reader.GetString(4),
+
+                });
+            }
+
+            return users;
+        }
+
+        public User? GetById(int id)
+        {
+            string query = "SELECT * FROM Users WHERE Id = @idPlaceholder";
+            var parameters = new[] { new SqlParameter("@idPlaceholder", id) };
+            using var reader = _db.ExecuteReader(query, parameters);
+
+            if (reader.Read())
+            {
+                var user = new User
+                {
+                    Id = reader.GetInt32(0),
+                    DateOfBirth = reader.GetDateTime(1),
+                    Name = reader.GetString(2),
+                    Surname = reader.GetString(3),
+                    Email = reader.GetString(4),
+                };
+
+                return user;
+            }
+
+            return null;
+        }
+
+        public int AddUser(User user)
+        {
+            string query = "INSERT INTO Users (DateOfBirth, Name, Surname, Email)" +
+                "VALUES (@datePlaceholder, @namePlaceholder, @surnamePlaceholder, @emailPlaceholder)";
+            
+            var parameters = new[] { new SqlParameter("@datePlaceholder", user.DateOfBirth),
+            new SqlParameter("@namePlaceholder", user.Name), new SqlParameter("@surnamePlaceholder", user.Surname),
+            new SqlParameter("@emailPlaceholder", user.Email)};
+
+            return _db.ExecuteNonQuery(query, parameters);
+        }
+
+        public int UpdateUser(User user)
+        {
+            string query = "UPDATE Users SET DateOfBirth = @datePlaceholder," +
+                "Name = @namePlaceholder, Surname = @surnamePlaceholder, Email = @emailPlaceholder" +
+                "WHERE Id = @idPlaceholder";
+
+            var parameters = new[]
+            {
+                new SqlParameter("@idPlaceholder", user.Id),
+                new SqlParameter("@datePlaceholder", user.DateOfBirth),
+                new SqlParameter("@namePlaceholder", user.Name),
+                new SqlParameter("@surnamePlaceholder", user.Surname),
+                new SqlParameter("@emailPlaceholder", user.Email)
+
+            };
+
+            return _db.ExecuteNonQuery(query, parameters);
+        }
+
+        public int DeleteUser(int id)
+        {
+            string query = "DELETE FROM Users WHERE Id = @idPlaceholder";
+
+            var parameters = new[]
+            {
+                new SqlParameter("@idPlaceholder", id)
+
+            };
+
+            return _db.ExecuteNonQuery(query, parameters);
+        }
+    }
+}
