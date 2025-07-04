b8fdf9e73c56dddad317da363d8826a862d6a8f9
Stefano Olivero <s.olivero.3215@vallauri.edu>
2025-07-04 23:57:12 +0200

    Created repository, controller and view of Booking
    The GetAll method of BookingRepository works but Booking's table is empty, so there isn't any record to show

---
+++ Library/Library.Data/Repos/BookingRepository.cs
@@ -0,0 +1,53 @@
+﻿using Library.Core.Models;
+
+namespace Library.Data.Repos
+{
+    public class BookingRepository : ICrud<Booking>
+    {
+        private readonly Database _db;
+
+        public BookingRepository(string connStr)
+        {
+            _db = new Database(connStr);
+        }
+
+        public List<Booking> GetAll()
+        {
+            var bookings = new List<Booking>();
+            string query = "SELECT * FROM Bookings";
+            using var reader = _db.ExecuteReader(query);
+
+            while (reader.Read())
+            {
+                bookings.Add(new Booking()
+                {
+                    Id = reader.GetInt32(0),
+                    UserId = reader.GetInt32(1),
+                    BookId = reader.GetInt32(2)
+                });
+            }
+
+            return bookings;
+        }
+
+        public Booking? GetById(int id)
+        {
+            return new Booking();
+        }
+
+        public int Add(Booking booking)
+        {
+            return 1;
+        }
+
+        public int Update(Booking booking)
+        {
+            return 1;
+        }
+
+        public int Delete(int id)
+        {
+            return 1;
+        }
+    }
+}
---
+++ Library/Library.Web/Controllers/BookingController.cs
@@ -0,0 +1,23 @@
+﻿using Library.Data.Repos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Web.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly BookingRepository _repo;
+
+        public BookingController(IConfiguration configuration)
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+            _repo = new BookingRepository(connStr);
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Bookings";
+            var bookings = _repo.GetAll();
+            return View(bookings);
+        }
+    }
+}
