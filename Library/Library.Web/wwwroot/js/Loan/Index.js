47604ec4dfe8f6151dff50530603011c11d7e87d
StefanoOlivero07 <s.olivero.3215@vallauri.edu>
2025-07-18 14:39:08 +0200

    Add jQuery library and minor changes

---
+++ Library/Library.Web/wwwroot/js/Loan/Index.js
@@ -0,0 +1,13 @@
+﻿"use strict"
+
+$(document).ready(function () {
+    const lstUsers = $("select");
+    const btnView = $("button");
+
+    lstUsers.prop("selectedIndex", -1);
+    btnView.prop("disabled", true);
+
+    lstUsers.on("change", () => {
+        btnView.prop("disabled", false);
+    });
+});
